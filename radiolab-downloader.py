4178e1aace28a43ba74b476caab2225161d64d42
Paul <43993770+robotoPaul@users.noreply.github.com>
2022-02-25 18:56:25 +0100

    Added  white and black list feature

--- radiolab-downloader.py
+++ radiolab-downloader.py
@@ -10,6 +10,9 @@ empty_kill_threshold = 10                       # How many pages with no episode
 start_page = 1
 end_page = 1000
 
+title_has_to_include = []                       # Only download episodes that contain any of these strings         example: ["bilbo","hobbyte"]
+title_does_not_contain = []                     # Exclude episodes that contain any of these strings              example: ["gandalf","gray"]
+
 def get_url_content(url):
     return requests.get(url).text
 
@@ -42,16 +45,34 @@ def check_existing_episode(title):
 def main():
     episodes = []
     empty_pages = 0
+    
+
     for i in range(start_page, end_page):
+        skip = False
         print("Page " + str(i))
         episodes = get_episode_urls(url_main, url_episodes + "/" + str(i))
+
         if(episodes == []):
             empty_pages += 1
+
         if(empty_kill_threshold<=empty_pages):
             print("Empty pages threshold reached")
             break
+
         for episode in episodes:
             title = episode[0]
+            for string in title_has_to_include:
+                if string not in title:
+                    skip = True
+                else:
+                    print(string+"    "+title)
+            for string in title_does_not_contain:
+                if string in title:
+                    skip = True
+            
+            if(skip):
+                continue
+
             if(check_existing_episode(title)):
                 print("Skipping existing episode " + title)
                 continue
